4b2745bfa07e749313f0aa5573b3bf8ec4002615
hupinimi <77345156+hupinimi@users.noreply.github.com>
2021-03-15 14:24:25 +0200

    Add files via upload

--- BottleDispenser.java
+++ BottleDispenser.java
@@ -1,17 +1,12 @@
 package com.example.ht8_2;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.os.Bundle;
-import android.os.Bundle;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.view.View;
 
 
 public class BottleDispenser extends AppCompatActivity {
@@ -32,39 +27,39 @@ public class BottleDispenser extends AppCompatActivity {
         catalogShower = (Button) findViewById(R.id.catalogButton);
         returnCash = (Button) findViewById(R.id.returnMoney);
         screenView = (TextView) findViewById(R.id.screenView);
-
-
         moneyAdder = (Button) findViewById(R.id.addMoney);
 
-        moneyAdder.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                money += 1;
-                screenView.setText("Klink! Added more money!");
+        moneyAdder.setOnClickListener(v -> {
+            money += 1;
+            screenView.setText("Klink! Added more money!");
+        });
+
+
+        bottleBuyer.setOnClickListener(v -> {
+            if (money < 1.8) {
+                screenView.setText("Add money first!");
+            } else {
+                if (bottles > 0) {
+                    bottles -= 1;
+                    money = money - 1.8;
+                    screenView.setText("KACHUNK! " + bottle_array.get(bottles).getName() + " came out of the dispenser!");
+
+                } else {
+                    screenView.setText("Out of bottles!");
+                }
             }
         });
 
-        bottleBuyer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                buyBottle(v);
-            }
+        returnCash.setOnClickListener(v -> {
+            money = 0;
+            screenView.setText("Klink klink. Money came out!");
         });
 
-        returnCash.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                returnMoney(v);
+        catalogShower.setOnClickListener(v -> {
+            for(int i = 1; i<= bottles; i++) {
+                screenView.setText(i +". " + "Name: Pepsi Max\n\tSize: 0.5	Price: 1.8");
             }
         });
-
-        catalogShower.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                catalog(v);
-            }
-        });
-
     }
 
     private int bottles;
@@ -87,36 +82,6 @@ public class BottleDispenser extends AppCompatActivity {
 
         }
 
-        //Scanner skanneri = new Scanner(System.in);
-
-        //while (true) {
-            //System.out.println("");
-            //System.out.print("*** BOTTLE DISPENSER ***\n1) Add money to the machine\n2) Buy a bottle\n3) Take money out\n4) List bottles in the dispenser\n0) End\n");
-            //System.out.print("Your choice: ");
-            //int num = skanneri.nextInt();
-
-            //if (num == 4) {
-                //System.out.print("1. Name: Pepsi Max\n\tSize: 0.5	Price: 1.8\n2. Name: Pepsi Max\n\tSize: 0.5	Price: 1.8\n3. Name: Pepsi Max\n\tSize: 0.5	Price: 1.8\n4. Name: Pepsi Max\n\tSize: 0.5	Price: 1.8\n");
-                //catalog();
-
-            //} else if (num == 1) {
-                //addMoney();
-
-            //} else if (num == 2 ) {
-                //buyBottle();
-
-            //}else if (num == 3) {
-               // returnMoney();
-
-            //}else if (num == 0) {
-                //break;
-
-            //}else {
-                //System.out.println("Huono numero.");
-            //}
-
-        //}
-        //skanneri.close();
     }
 
     public void catalog(View v) {
